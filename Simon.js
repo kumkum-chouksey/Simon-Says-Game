0cc01dd97fed829db0586ddd8e6c80edc024be01
kumkum-chouksey <choukseykumkum00@gmail.com>
2026-03-22 17:54:38 +0530

    working level1

--- Simon.js
+++ Simon.js
@@ -14,12 +14,24 @@ document.addEventListener("keypress", function() {
  }
 });
 
+function checkAns() {
+  let idx=level-1;
+  if(userSq[idx]===gameSq[idx]) {
+      console.log("same value");
+  } else {
+    h3.innerText="Game Over";
+    console.log("Game Over");
+  }
+}
+
 function levelUp() {
   level++;
   h3.innerText=`Level ${level}`;
 
   let randomIdx=Math.floor(Math.random()*3);
   let randomColor=btn[randomIdx];
+  gameSq.push(randomColor);
+  console.log(gameSq);
   let randomBtn=document.querySelector(`.${randomColor}`);
   btnFlash(randomBtn);
 }
@@ -27,16 +39,19 @@ function btnFlash(btn) {
  btn.classList.add("flash");
  setTimeout(function() {
    btn.classList.remove("flash");
- },200);
+ },100);
 }
 
-function btnPress() {
-  console.log("btn was pressed");
+function btnPress(Btn) {
+  userColor=Btn.getAttribute("id");
+  userSq.push(userColor);
+  checkAns();
 }
+
 let allBtn=document.querySelectorAll(".btn");
 for(let Btn of allBtn){
 Btn.addEventListener("click", function() {
-   btnPress();
+   btnPress(Btn);
    btnFlash(Btn);
 });
 }
